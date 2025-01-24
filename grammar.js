0a6a51ea544cdc50fc47ede0bc7a5f9da7965148
Kensaa <54020933+Kensaa@users.noreply.github.com>
2025-01-24 13:48:49 +0100

    working grammar

--- grammar.js
+++ grammar.js
@@ -10,8 +10,33 @@
 module.exports = grammar({
   name: "y86",
 
+  extras: ($) => [/[ \t]+/, $.comment],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
-  }
+    source_file: ($) => repeat($.line),
+
+    identifier: ($) => /[a-zA-Z][0-9a-zA-Z_]*/,
+    _newline: ($) => /[\n]+/,
+    // line: ($) => seq(choice($.instruction, $.label, $.directive), $._newline),
+    line: ($) =>
+      seq(
+        optional($.label),
+        optional(choice($.instruction, $.directive)),
+        $._newline
+      ),
+
+    label: ($) => seq($.identifier, ":"),
+    instruction: ($) => seq($.identifier, optional($.arg_list)),
+    directive: ($) => seq(".", $.identifier, $.number),
+
+    arg_list: ($) => seq($.arg, optional(seq(",", $.arg))),
+    arg: ($) => choice($.index, $.number, $.identifier, $.register),
+
+    number: ($) => /(0x[0-9a-fA-F]+)|(-?[0-9]+)/,
+    register: ($) =>
+      seq("%", choice("eax", "ecx", "edx", "ebx", "esi", "edi", "esp", "ebp")),
+    index: ($) => seq($.number, "(", $.register, ")"),
+
+    comment: ($) => /#.*/,
+  },
 });
